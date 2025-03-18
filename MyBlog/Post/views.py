e71f0a411b964b34495caa186c3e8ee6161362fe
DmRafaule <chedrden@gmail.com>
2025-03-18 07:53:31 +0300

    Some minor improvements of interlinking the articles

--- MyBlog/Post/views.py
+++ MyBlog/Post/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext as _
 from django.template.response import TemplateResponse
+from bs4 import BeautifulSoup
 
 
 def article(request, post_slug):
@@ -25,6 +26,32 @@ def article(request, post_slug):
         sim_post_doc = loaded_template.render(context, request)
 
     context.update({'post': post})
+    # Get time to read
+    with post.template.open('r') as file:
+        soup = BeautifulSoup(file.read(), features="lxml")
+        text = soup.get_text()
+        words_in_text = len(text.split())
+        time_to_read = round(words_in_text/240)
+    context.update({'time_to_read': time_to_read})
+    
+    # Get next and previos posts
+    previous_id=Post_M.Article.objects.filter(
+         id__lt=post.id,
+     ).order_by("-id").values_list("id")[:1],
+    next_id=Post_M.Article.objects.filter(
+         id__gt=post.id,
+    ).order_by("id").values_list("id")[:1]
+    # Check if there is no next element
+    if len(next_id) > 0:
+        id = next_id[0][0]
+        next_post = Post_M.Article.objects.get(id=id)
+        context.update({'next_post': next_post})
+    # Check if there is no prev element
+    if len(previous_id[0]) > 0:
+        id = previous_id[0][0][0]
+        prev_post = Post_M.Article.objects.get(id=id)
+        context.update({'prev_post': prev_post})
+    
     context.update({'sim_post_doc': sim_post_doc})
     context.update({'downloadables': downloadables})
     context.update({'images': images})
