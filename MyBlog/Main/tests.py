28571454c26dba7bc901e5dc820cc15200bab5ee
DmRafaule <ChedrDen@gmail.com>
2025-06-16 17:38:45 +0300

    Изменил тест для проверки рендеринга домашней страницы

--- MyBlog/Main/tests.py
+++ MyBlog/Main/tests.py
@@ -1,7 +1,12 @@
+from django.urls import reverse
+from django.http import HttpRequest
 from django.test import TestCase
+from .views import home
 
-class SmokeTest(TestCase):
-    '''тест на токсичность'''
-    def test_bad_maths(self):
-        '''тест: неправильные математические расчеты'''
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_home_page_return_correct_html(self):
+        # Тест на правильно возвращаемый контент
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'Main/home.html')
