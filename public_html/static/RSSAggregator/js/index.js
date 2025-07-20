697551629615a05aa9b9fefa2a34de02af21f5d3
Tima <70476470+DmRafaule@users.noreply.github.com>
2025-07-21 01:30:24 +0300

    Delete public_html directory

--- public_html/static/RSSAggregator/js/index.js
+++
@@ -1,2 +0,0 @@
-import './htmx.js';
-import './tabs.js'
--- public_html/static/toggle_nav.js
+++
@@ -1,1 +0,0 @@
-let menu_button=document.getElementById("menu_button"),header=document.getElementById("header"),header_height=header.getBoundingClientRect().height+"px";function toggleMenu(){var e=document.getElementById("nav");e.classList.toggle("active_nav"),e.style.top=header_height}menu_button.addEventListener("click",toggleMenu)
