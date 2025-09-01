0e650d57f7cc80cd7ab07744cfb856c0eea1237f
DmRafaule <ChedrDen@gmail.com>
2025-09-01 20:39:33 +0300

    4-й Этап оптимизации. Очистка от лишнего мусора(CSS,JS,IMG).

--- Website/Main/static/sticky.js
+++
@@ -1,9 +0,0 @@
-/* Update main tag in the way that it appears to have some 
- * padding on top. Padding on top of it is a height of header tag
- * */
-function updateMain() {
-	var header = document.getElementById("header")
-	var main   = document.getElementById("main")
-	main.style.paddingTop = header.getBoundingClientRect().height  + "px"
-} 
-window.addEventListener('DOMContentLoaded', updateMain)
